d3bcfbfdfa038944152f4e3a60bcc3d15517b647
David Rivas <105255097+davidfrivas@users.noreply.github.com>
2025-07-07 12:22:08 -0400

    Implement react router Links & Outlet

---
+++ client/src/components/pages/Navbar.js
@@ -0,0 +1,51 @@
+import { Outlet, Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <img 
+              src="/assets/mouse.svg" 
+              alt="Logo" 
+              width="30" 
+              height="24" 
+              className="d-inline-block align-text-top me-2"
+            />
+            Mouse Colony Manager
+          </Link>
+          <button 
+            className="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Profile</a>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">Register</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="main-content">
+        <Outlet/>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
