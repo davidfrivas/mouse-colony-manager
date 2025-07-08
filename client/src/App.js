7ffaf3f7d8c9ee81c8d6e828a73b8fdc49d4465b
David Rivas <105255097+davidfrivas@users.noreply.github.com>
2025-07-07 20:15:46 -0400

    Add route to Profile component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/pages/Navbar.js';
 import LoginForm from './components/pages/LoginForm.js';
 import RegisterForm from './components/pages/RegisterForm.js';
+import Profile from './components/pages/Profile.js'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Navbar />}>
               <Route path="register" element={<RegisterForm />}/>
               <Route path="login" element={<LoginForm />}/>
+              <Route path="profile" element={<Profile />}/>
           </Route>
         </Routes>
       </BrowserRouter>
